efe145c343b9eff67ab72d0e2ab2ff743726cb02
sperrye <sperrye@gmail.com>
2017-03-02 11:11:09 -0800

    sage

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,17 +1,23 @@
 console.log('hello world');
 
+document.getElementById('changeColor').addEventListener('click', function() {
+  console.log('you clicked me');
+});
+
 function validateForm() {
-  console.log('validating form')
   var name = document.getElementById('nameInput').value;
   var species = document.getElementById('speciesInput').value;
   var age = document.getElementById('ageInput').value;
 
-  if (!name.length && !species.length && !age.length) {
-    console.log('successfully added');
-    return true;
-  }
-  else {
-    console.log('all fields required.');
-    return false;
-  }
+    if (!name.length || !species.length || !age.length) {
+      alert('All fields required.');
+      return false;
+    }
+
+    if (isNaN(parseInt(age))) {
+      alert('Age must be a number');
+      return false;
+    }
+
+  return true;
 }
